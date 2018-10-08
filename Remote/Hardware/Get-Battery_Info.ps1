505aa59963d3db75b344e9d1a8ec129b0051c930
Dwight Brenner <D.Brenner@truepointwealth.com>
2018-10-07 22:16:10 -0400

    More functions

---
+++ Remote/Hardware/Get-Battery_Info.ps1
@@ -0,0 +1,101 @@
+#Execute
+###############################################################################################
+# Get Battery Info
+###############################################################################################
+# Path to list of computer-names and user-names
+$pcList = $env:COMPUTERSLIST
+$adminName = $env:adminName
+if ($cred) {} else { $cred = Get-Credential $adminName }
+#######################################*Begin Script*##########################################
+
+$user = @()
+$computer = @()
+$batteries = @()
+
+#import list of usernames and computernames from .csv
+#save into two arrays
+Import-Csv $pcList | ForEach-Object {
+        $user += $_.user
+        $computer += $_.computer
+}
+
+#get my computername
+$MycompName = [string]((Get-WmiObject Win32_ComputerSystem).Name)
+
+# Helper functions
+$scriptBlock = [ScriptBlock]::Create({Get-WmiObject -Class Win32_Battery})
+# (Get-Win32_Battery -wmibattery (Get-WmiObject -Class Win32_Battery)) | Format-Table
+function Get-Win32_Battery ($wmibattery){
+    $DeviceID = [string]($wmibattery.DeviceID)
+    $TimeOnBattery = [string]($wmibattery.TimeOnBattery)
+    $EstimatedChargeRemaining = [int]($wmibattery.EstimatedChargeRemaining)
+    $EstimatedRunTime = [string]($wmibattery.EstimatedRunTime)
+    $Availability = Switch ([string]($wmibattery.Availability)){
+        1 {"Other";break}
+        2 {"Unknown";break}
+        3 {"Running or Full Power";break}
+        4 {"Warning";break}
+        5 {"In Test";break}
+        6 {"Not Applicable";break}
+        7 {"Power Off";break}
+        8 {"Off Line";break}
+        9 {"Off Duty";break}
+        10 {"Degraded";break}
+        11 {"Not Installed";break}
+        12 {"Install Error";break}
+        13 {"Power Save - Unknown";break}
+        14 {"Power Save - Low Power Mode";break}
+        15 {"Power Save - Standby";break}
+        16 {"Power Cycle";break}
+        17 {"Power Save - Warning";break}
+    }
+    $BatteryStatus = Switch ([string]($wmibattery.Availability)){
+        1 {"Discharging";break}
+        2 {"On A/C";break}
+        3 {"Fully Charged";break}
+        4 {"Low";break}
+        5 {"Critical";break}
+        6 {"Charging";break}
+        7 {"Charging High";break}
+        8 {"Charging Low";break}
+        9 {"Charging Critical";break}
+        10 {"Undefined";break}
+        11 {"Partially Charged";break}
+    }
+        
+    $properties = @{
+        'Computer'=$icomputer;
+        'User'=$iuser;
+        'DeviceID'=$DeviceID;
+        'TimeOnBattery'=[INT]$TimeOnBattery;
+        'EstChargeRemaining'=[INT]$EstimatedChargeRemaining;
+        'EstRunTime'=[INT]$EstimatedRunTime;
+        'Availability'=$Availability;
+        'Status'=$BatteryStatus}
+    $battery = New-Object PSObject -Property $properties
+    return $battery
+}
+# Main loop
+for ( $i=0; $i -le ($user.Length - 1); $i++) {
+	$iuser = $user[$i]
+	$icomputer = $computer[$i] 
+	$currentOpp = "Querying battery info on " + $icomputer + " (" + $iuser + "`'s computer)"
+	echo $currentOpp
+    if (!(Test-Connection -ComputerName $icomputer -BufferSize 16 -Count 1 -ErrorAction 0 -Quiet)) { echo "Unreachable!" } else {
+        if ($icomputer -ne $MycompName){
+            $batteryInfo = Invoke-Command -ComputerName $icomputer -Credential $cred -ScriptBlock $scriptBlock
+        } else {
+            $batteryInfo = Get-WmiObject -Class Win32_Battery
+        }
+        $batteries += (Get-Win32_Battery -wmibattery $batteryInfo)
+    }
+}
+Get-PSSession | Remove-PSSession
+
+## Show all battery data
+#$batteries | Select Computer, User, DeviceID, EstRunTime, EstChargeRemaining, Status | Out-GridView
+
+## Show all battery data where est runtime < 30
+#$batteries | Where-Object -Property EstRunTime -lt 30 | Where-Object -Property EstChargeRemaining -gt 0 | Select Computer, User, DeviceID, EstRunTime, EstChargeRemaining, Status | Out-GridView
+$batteries | Sort-Object -Property User |Where-Object -Property EstChargeRemaining -gt 0 | Select Computer, User, DeviceID, EstRunTime, EstChargeRemaining, Status | Out-GridView
+Pause
---
+++ Remote/Hardware/Get-PC_Hardware_Info.ps1
@@ -0,0 +1,163 @@
+#Execute
+###############################################################################################
+# Query Computer Specs
+# Modified to utilize jobs (parallel data gathering)
+###############################################################################################
+# Path to list of computer-names and user-names
+$pcList = $env:COMPUTERSLIST
+$adminName = $env:adminName
+if ($cred) {} else { $cred = Get-Credential $adminName }
+#######################################*Begin Script*##########################################
+$host.ui.RawUI.WindowTitle = "Getting PC info..."
+$user = @()
+$computer = @()
+
+#import list of usernames and computernames from .csv
+#save into two arrays
+Import-Csv $pcList | ForEach-Object {
+        $user += $_.user
+        $computer += $_.computer
+}
+
+function computerInfo {
+    [CmdletBinding()] Param(
+    [Parameter(Position = 0, Mandatory = $True)]
+    [String]$pcname,
+    [Parameter(Position = 1, Mandatory = $True)]
+    [String]$uname
+    )
+    $runningBlockFunction = [ScriptBlock]::Create({
+    function getInfo {
+        [CmdletBinding()] Param(
+        [Parameter(Position = 0, Mandatory = $True)]
+        [String]$Computer,
+        [Parameter(Position = 1, Mandatory = $True)]
+        [String]$User
+        )
+        $ErrorActionPreference = 'SilentlyContinue' ## Suppress errors
+        $PCinfo = Get-WmiObject Win32_ComputerSystem
+        $BIOSinfo = Get-WmiObject Win32_Bios
+        $OSinfo = Get-WmiObject Win32_OperatingSystem
+        $CPUinfo = Get-WmiObject Win32_Processor
+        $GPUinfo = Get-WmiObject Win32_VideoController
+        $HDDinfo = Get-WmiObject Win32_DiskDrive
+        $freeSpace = Get-WmiObject Win32_LogicalDisk -Filter { DeviceID = "C:" }
+        [String]$TBLfmt = "MBR"
+        if ((Get-WmiObject Win32_DiskPartition | Where-Object BootPartition -eq $true | Select-Object Type).Type -like "*GPT*"){ [String]$TBLfmt = "GPT" }
+        $installedMemory = $PCinfo.TotalPhysicalMemory
+        $installedMemoryGB = [Math]::Ceiling($installedMemory / [Math]::Pow(1024,3))
+        if ($OSinfo.Version -match "6.1") {
+            $OS = "Windows 7"
+        } 
+        if ($OSinfo.Version -match "6.3") {
+            $OS = "Windows 8.1"
+        }
+        if ($OSinfo.Version -match "10.0") {
+            #$OS = "Windows 10"
+            $OS = [String]($OSinfo.Version)
+        }
+        $PCmodel = $PCinfo.Model
+        if ($PCmodel.Length -ge 14) {
+            while ($PCmodel -like "*  *") {
+                $PCmodel = $PCmodel -replace "  ", "" #Remove whitespace
+            }
+            if ($PCmodel.Length -ge 14) {
+                $PCmodel = $PCmodel.Substring(0,13) #restrict to first 13 chars
+            }
+        }
+        $CPU = $CPUinfo.Name
+        while (($CPU -like "*  *") -or ($CPU -like "*`t*")) {
+            $CPU = $CPU -replace "  ", " "
+            $CPU = $CPU -replace "`t", " "
+        }
+        $cpuArch = [String]$OSinfo.OSArchitecture
+        $memString = [String]$installedMemoryGB + "GB"
+        $hdd = @()
+        $HDDinfo | ForEach-Object { $hdd += ([String]([Math]::Ceiling($_.Size / [Math]::Pow(1024,3))) + "GB") }
+        $GPU = $GPUinfo.Name
+        $BIOS = $BIOSinfo.SMBIOSBIOSVersion
+        $pctfree = @()
+        $freeSpace | %{ if($_.Size -gt 0){ $pctfree += [Math]::Round(($_.FreeSpace / $_.Size)*100) }}
+        If ($memString -notlike "0*") {
+            $props = @{
+                Computer = $Computer
+                User = $User
+                Model = $PCmodel
+                BIOS = $BIOS
+                DISK = $TBLfmt
+                CPU = $CPU
+                RAM = $memString
+                HDD = $hdd
+                FREE = $pctfree
+                OS = $OS
+                Arch = $cpuArch
+                GPU = $GPU
+            }
+            $computerInfo = @()
+            $computerInfo = New-Object PSObject -Property $props
+            return $computerInfo
+        }
+    }
+
+    })
+    $runningBlock = [ScriptBlock]::Create($runningBlockFunction.ToString() + "getInfo -Computer $pcname -User $uname")
+
+    if (!(Test-Connection -ComputerName $pcname -BufferSize 16 -Count 1 -ErrorAction 0 -Quiet)) { 
+        Write-Host "......Unreachable" -ForegroundColor Red
+    } else {
+        if ($pcname -ne $env:COMPUTERNAME){
+            Invoke-Command -ComputerName $pcname -Credential $cred -ScriptBlock $runningBlock -AsJob
+        } else {
+            Start-Job -ScriptBlock $runningBlock
+        }
+    }
+
+}
+
+##loop through arrays to get computer info
+for ( $i=0; $i -le ($user.Length - 1); $i++) {
+	$iuser = $user[$i]
+	$icomputer = $computer[$i]
+    $currentOpp = "Checking PC info on " + $icomputer + " " + $iuser
+    echo $currentOpp
+    computerInfo -pcname $icomputer -uname $iuser
+
+}
+
+$count = 0
+$njobs = (Get-Job -State Running).count
+Clear
+Write-Host -NoNewline "Waiting for most jobs ($njobs) to complete" -ForegroundColor White
+## Wait for jobs to finish
+While (((Get-Job -State Running).count -gt 0) -and ($count -lt 60)){
+    $jobs = (Get-Job -State Running).count
+    if ($njobs -ne $jobs){
+        Clear
+        Write-Host -NoNewline "Waiting for most jobs ($jobs) to complete" -ForegroundColor White
+        for ($i = 0 ; $i -lt $count + 1 ; $i ++){ Write-Host -NoNewline "." -ForegroundColor White }
+        $njobs = $jobs
+    } else {
+        Write-Host -NoNewline "." -ForegroundColor White
+    }
+    Start-Sleep -Seconds 1 # Wait for any running jobs to finish
+    $count = $count + 1
+    if ((Get-Job -State Running).count -eq 0){$count = 60}
+}
+Clear
+
+## Return computerInfo to an array of objects
+$computerInfos = @()
+foreach ($job in (Get-Job -State Completed)){
+    $pcname = $job.Location
+    if ($pcname -eq "localhost"){ $pcname = $env:COMPUTERNAME }
+    $computerInfos += ((Get-Job -Name $job.Name) | Receive-Job)
+}
+Get-Job | Remove-Job
+
+# Array of objects has been stored in $computerInfos
+$computerInfos | Select Computer, User, Model, BIOS, DISK, CPU, Arch, RAM, HDD, FREE, OS, GPU | Out-GridView
+
+## Save computer info to XML
+#$computerInfos | Export-Clixml -Path infos.xml
+## Read computer info from XML
+#$computerInfos2 = Import-Clixml -Path infos.xml
